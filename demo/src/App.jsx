ca1b4b05ddfa3c94052a83a55b12b7333054cc9c
Adnanmuhammed8 <adnanmuhammed317@gmail.com>
2025-03-12 14:00:51 +0530

    react

---
+++ demo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  //js code
+  
+
+  return (
+    /// output - html
+    <>
+    <h1>REACT</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corporis hic sit voluptatem reiciendis facere ab libero fuga suscipit saepe praesentium, id provident harum quia quaerat eligendi repudiandae nulla nesciunt.</p>
+
+    </>
+  )
+}
+
+export default App
